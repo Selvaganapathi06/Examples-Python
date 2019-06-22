59eb3dce26fe23811ef360a762c44d3daff19557
Selvaganapathi06 <34333910+Selvaganapathi06@users.noreply.github.com>
2019-06-22 08:29:03 +0530

    Add files via upload

---
+++ Day2/day2.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#String formating1
+
+fname = 'Selva'
+lname = 'ganapathi'
+fullname = f"{fname}{lname}"
+print(fullname)
+
+
+# In[2]:
+
+
+#String formating2
+
+fname = 'Selva'
+lname = 'ganapathi'
+fullname = f"{fname} {lname}"
+print(fullname)
+
+
+# In[3]:
+
+
+#Adding whitespaces
+a = "Selvaganapathi"
+print(a)
+print("\t selvaganapathi")
+
+
+# In[5]:
+
+
+#new line
+print("programming languages: \n 1.c \n 2.c++ \n 3. Java")
+
+
+# In[6]:
+
+
+#remove whitespaces
+a = "selva         "
+print(a)
+print(a.strip())
+
+
+# In[7]:
+
+
+#operators
+x = 10
+y = 5
+
+c = x+y
+d = x-y
+e = x*y
+f = x/y
+g = x%y
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
+
+# In[8]:
+
+
+import this
+
+
+# In[ ]:
+
+
+
+
