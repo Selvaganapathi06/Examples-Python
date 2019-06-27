08b1f35681051237b68facca30287591d9449665
Selvaganapathi Sennan <34333910+Selvaganapathi06@users.noreply.github.com>
2019-06-27 07:53:38 +0530

    Add files via upload

---
+++ Day09/Day09.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#Dictionaries
+#Define Dictionaries
+ex1={'color':'red','points':5}
+#Access the value in Dictionaries
+print(ex1['color'])
+print(ex1['points'])
+
+
+# In[3]:
+
+
+#Dictionaries
+ex1={'color':'green','points':10}
+print(ex1['color'])
+print(ex1['points'])
+
+
+# In[ ]:
+
+
+
+
