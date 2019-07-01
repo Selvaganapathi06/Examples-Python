ccacf6b8ad2ea1857f31d8ff36aa619ce3b758bd
Selvaganapathi Sennan <34333910+Selvaganapathi06@users.noreply.github.com>
2019-07-01 08:07:24 +0530

    Add files via upload

---
+++ day12/day12.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#getting dynamic input
+msg = input("Tell me some thing: ")
+print(msg)
+
+
+# In[2]:
+
+
+name =input("enter your name: ")
+print(f"hello,{name}")
+
+
+# In[3]:
+
+
+#converting input data type
+age = input("enter your age: ")
+age1=int(age)
+if age1 >18:
+    print("eligible for voting")
+else:
+    print("Not eligible voting")
+            
+
+
+# In[ ]:
+
+
+
+
