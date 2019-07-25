6b57f4a2890e87d4694de19835a8b8ea9cb1369b
Selvaganapathi Sennan <34333910+Selvaganapathi06@users.noreply.github.com>
2019-07-25 08:49:18 +0530

    Add files via upload

---
+++ prime number/prime no.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+#prime no
+
+num = int(input("Enter a number: "))
+
+# prime numbers are greater than 1
+if num > 1:
+   # check for factors
+    for i in range(2,num):
+        if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+    else:
+            print(num,"is a prime number")
+       
+# if input number is less than
+# or equal to 1, it is not prime
+else:
+   print(num,"is not a prime number")
+
